d1b2707ae36e64032220811c927f168a4d9a488f
Neetesh-6 <114156149+Neetesh-6@users.noreply.github.com>
2022-09-26 16:49:26 +0530

    Add files via upload

---
+++ Tesla.java
@@ -0,0 +1,14 @@
+package corejava;
+public class Tesla extends car2
+{
+void drive()
+{
+system.out.println("drive safely");
+}
+public static void main(string[] args)
+{
+Tesla t= new Tesla();
+t.drive();
+t.gearchange();
+}
+}
