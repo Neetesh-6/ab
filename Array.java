76c3bf292a1d903ceb1fc6b3936e8e5f32cadd6d
Neetesh-6 <114156149+Neetesh-6@users.noreply.github.com>
2022-10-19 15:24:51 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,12 @@
+package CoreJava;
+
+public class Array {
+		public static void main(String[] args) {
+	        String[] name = { "Neetsh", "Vikram", "shivam", "vivek" };
+	        System.out.println(name[0]);
+	        System.out.println(name[1]);
+	        System.out.println(name[2]);
+	        System.out.println(name[3]);
+	}
+	
+}
